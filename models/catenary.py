c0433c67e2dc893119c9032e3e198679dfa00693
zartavr <zartavr@gmail.com>
2024-03-18 01:53:40 +0300

    [forward] Add catenary class

---
+++ models/catenary.py
@@ -0,0 +1,57 @@
+from numpy import cosh, sinh, sqrt
+import scipy.optimize as sp
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Catenary:
+    def __init__(self, d_support_length, d_support_height, length) -> None:
+        # ref docs/attachments/image.png
+        self.param_l = d_support_length
+        self.param_h = d_support_height
+        self.length = length
+        self.param_a = 0
+        self.B_BRACKET = [1e-6, 1 / self.param_l]
+
+    def set_catenary_length(self, length):
+        self.length = length
+
+    def set_param_a(self, a):
+        self.param_a = a
+
+    def catenary(self, x):
+        # if self.param_a == 0:
+        #     self.find_a()
+        if x == 0:
+            return 0
+        return self.param_a * cosh(x / self.param_a)
+
+    def __get_param_eq(self):
+        return sqrt(self.length**2 - self.param_h**2) / self.param_l
+
+    def __catenary_b(self, b):
+        param_eq = self.__get_param_eq()
+        return 2 * b * sinh(1 / (2 * b)) - param_eq
+
+    def catenary_a(self, b):
+        return b * self.param_l
+
+    def find_a(self, debug: bool = 0) -> float:
+        res = sp.root_scalar(self.__catenary_b, bracket=self.B_BRACKET, method="brentq")
+        
+        self.param_a = res.root
+
+        if debug:
+            print(f"a = {self.catenary_a(res.root)}")
+            print(f"iterations: {res.iterations}")
+
+            b_x = np.linspace(self.B_BRACKET[0], self.B_BRACKET[1], 1000)
+            b_y = np.array([self.__catenary_b(b) + self.__get_param_eq for b in b_x])
+            param_const = np.array([self.__get_param_eq for b in b_x])
+
+            plt.figure("a_param")
+            plt.plot(b_x, b_y)
+            plt.plot(b_x, param_const)
+            plt.ylim([0, 10])
+            plt.show()
+        return res.root
--- models/main.py
+++ models/main.py
@@ -1,5 +1,7 @@
-from filament import *
-from motor import *
+from filament import Filament
+from motor import Motor
+from catenary import Catenary
+
 import scipy.optimize as sp
 from numpy import sinh, sqrt, cosh
 import numpy as np
@@ -8,48 +10,6 @@ import matplotlib
 
 matplotlib.use("TkAgg")
 from drawnow import *
-
-
-param_l = 1
-param_h = 0
-length = param_l
-param_eq = sqrt(length**2 - param_h**2) / param_l
-
-B_BRACKET = [1e-3, 2]
-
-
-def catenary(x, a):
-    if x == 0:
-        return 0
-    return a * cosh(x / a)
-
-
-def catenary_b(b):
-    return 2 * b * sinh(1 / (2 * b)) - param_eq
-
-
-def catenary_a(b):
-    return b * param_l
-
-
-def find_a(debug: bool = 0) -> float:
-    res = sp.root_scalar(catenary_b, bracket=B_BRACKET, method="brentq")
-
-    if debug:
-        print(f"a = {catenary_a(res.root)}")
-        print(f"iterations: {res.iterations}")
-
-        b_x = np.linspace(B_BRACKET[0], B_BRACKET[1], 1000)
-        b_y = np.array([catenary_b(b) + param_eq for b in b_x])
-        param_const = np.array([param_eq for b in b_x])
-
-        plt.figure("a_param")
-        plt.plot(b_x, b_y)
-        plt.plot(b_x, param_const)
-        plt.ylim([0, 10])
-        plt.show()
-    return res.root
-
 
 catenary_x = []
 catenary_y = []
@@ -61,7 +21,7 @@ def makeFig():  # Create a function that makes our desired plot
     plt.xlabel("x")  # Set ylabels
     plt.ylabel("y")  # Set ylabels
     plt.ylim([-10, 1])
-    plt.plot(catenary_x, catenary_y, label='length = {0:.2f}'.format(length))
+    plt.plot(catenary_x, catenary_y, label="length = {0:.2f}".format(length))
 
 
 if __name__ == "__main__":
@@ -70,28 +30,32 @@ if __name__ == "__main__":
     filament = Filament(2)
     extruder = Motor(0, 0.1)
     winder = Motor(0, 0.1)
-    
+    catenary = Catenary(1, 0, 1.5)
+
     extruder.set_velocity(0.5)
 
     while True:
         time_tick += 1
-        if length >= 18:
+        if catenary.length >= 18:
             winder.set_velocity(1)
-        if length <= 2:
+        if catenary.length <= 2:
             winder.set_velocity(0.1)
         delta_velocity = extruder.get_velocity() - winder.get_velocity()
 
         filament.set_velocity(delta_velocity, 1)
         length = filament.get_length()
         print(f"len: {length:.2f}", end="\t")
-        param_eq = sqrt(length**2 - param_h**2) / param_l
-        param_a = find_a()
-        print(f"param a: {param_a:.2f}", end="\t")
+        catenary.set_catenary_length(length)
+        catenary.find_a()
+        print(f"param a: {catenary.param_a}", end="\t")
         print()
 
-        catenary_x = np.linspace(-param_l/2, param_l/2, 100)
+        catenary_x = np.linspace(-catenary.param_l / 2, catenary.param_l / 2, 100)
         catenary_y = np.array(
-            [catenary(x, param_a) - catenary(catenary_x[0], param_a) for x in catenary_x]
+            [
+                catenary.catenary(x) - catenary.catenary(catenary_x[0])
+                for x in catenary_x
+            ]
         )
 
         drawnow(makeFig)
